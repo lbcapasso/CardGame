bdcee6d91212ee636baac19bdad3d1142821182f
lcapasso <lucas.capasso@menloschool.org>
2024-11-18 09:37:29 -0800

    Finished the Card Class

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,44 @@
 public class Card
 {
+    private int value;
+    private String suit;
+    private String rank;
+    public Card(int v, String s, String r)
+    {
+        value = v;
+        suit = s;
+        rank = r;
+    }
 
+
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    public String getSuit()
+    {
+        return suit;
+    }
+
+    public String getRank()
+    {
+        return rank;
+    }
+
+    public void setRank(String rank)
+    {
+        this.rank = rank;
+    }
+
+    public void setSuit(String suit)
+    {
+        this.suit = suit;
+    }
+
+    public void setValue(int value)
+    {
+        this.value = value;
+    }
 }
