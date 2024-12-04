d5d576db1886e543bd8f6aff533f3f187d710a7d
lcapasso <lucas.capasso@menloschool.org>
2024-12-04 09:57:31 -0800

    worked a lot on the player class.

--- src/Player.java
+++ src/Player.java
@@ -2,8 +2,35 @@ import java.util.ArrayList;
 
 public class Player
 {
-    private ArrayList<Card> hand= new ArrayList<Card>();
+    private ArrayList<Card> hand = new ArrayList<Card>();
     private boolean lives;
     private int points;
-    private
+    private String name;
+
+    public Player (String newName)
+    {
+        name = newName;
+        points = 0;
+        hand = null;
+    }
+    public Player (String nName, ArrayList<Card> newHand)
+    {
+        name = nName;
+        points = 0;
+        hand = newHand;
+    }
+    public void addPoints(int newPoints )
+    {
+        points = points + newPoints;
+    }
+
+    public void addCards(Card newCard)
+    {
+        hand.add(newCard);
+    }
+
+    @Override
+    public String toString() {
+        return name + " has " + points + " points " + "\n" + name + "'s cards:" + hand;
+    }
 }
