015ba936ee5807959f35ed2bd6082d5f8aa08639
lcapasso <lucas.capasso@menloschool.org>
2024-12-12 12:37:38 -0800

    started on the actual game

--- src/Player.java
+++ src/Player.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Player
 {
     private ArrayList<Card> hand = new ArrayList<Card>();
-    private boolean lives;
+    private int lives;
     private int points;
     private String name;
 
@@ -11,22 +11,55 @@ public class Player
     {
         name = newName;
         points = 0;
+        lives = 3;
         hand = null;
     }
     public Player (String nName, ArrayList<Card> newHand)
     {
         name = nName;
         points = 0;
+        lives = 3;
         hand = newHand;
+    }
+
+    public Card getCard(int location)
+    {
+        return hand.get(location);
+    }
+
+    public int getHandSize()
+    {
+        return hand.size();
     }
     public void addPoints(int newPoints )
     {
         points = points + newPoints;
     }
 
+    public void removeCard(int location)
+    {
+        hand.remove(location);
+    }
     public void addCards(Card newCard)
     {
         hand.add(newCard);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+
+    public ArrayList<Card> getHand() {
+        return hand;
     }
 
     @Override
